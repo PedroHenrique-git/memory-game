2c13edbf9465766be0edec199b5274961e97be68
PedroHenrique-git <pedromf34@outlook.com>
2022-02-09 22:02:15 -0300

    feat: finalizing project

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,14 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Game from './components/Game/Game';
 
 const App = () => {
-  return <Game />;
+  return (
+    <>
+      <Game minGame={14} maxGame={64} timeToShowImage={1000} />;
+      <ToastContainer />
+    </>
+  );
 };
 
 export default App;
--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'preact/hooks';
+import { toast } from 'react-toastify';
 import { v4 as uuid } from 'uuid';
 import generateRandomEmoji from '../../utils/generateRandomEmoji';
 import { generateRandomNumber } from '../../utils/generateRandomNumber';
@@ -10,20 +11,24 @@ type Card = {
   emoji: string;
 };
 
-const Game = () => {
+type GameProps = {
+  minGame: number;
+  maxGame: number;
+  timeToShowImage: number;
+};
+
+const Game = ({ minGame, maxGame, timeToShowImage }: GameProps) => {
   const [cards, setCards] = useState<Card[]>([]);
-  const [difficult, setDifficult] = useState(generateRandomNumber(24, 64));
+  const [difficult, setDifficult] = useState(
+    generateRandomNumber(minGame, maxGame),
+  );
   const [selectedCards, setSelectedCards] = useState<Card[]>([]);
   const [showImages, setShowImages] = useState<boolean[]>([]);
 
   const handleClickCard = (index: number, card: Card) => {
-    const newImages = showImages.map((v, i) => {
-      if (index === i) return true;
-      return v;
-    });
-
+    const newShowImages = showImages.map((v, i) => (i === index ? !v : v));
     setSelectedCards((prevState) => [...prevState, card]);
-    setShowImages(newImages);
+    setShowImages(newShowImages);
   };
 
   useEffect(() => {
@@ -32,9 +37,10 @@ const Game = () => {
 
       for (let i = 0; i < difficult; i += 1) {
         const card = { id: uuid(), emoji: generateRandomEmoji() };
-        setCards((prevState) => Array.from(new Set([...prevState, card])));
+        setCards((prevState) => [...prevState, card]);
       }
 
+      setCards((prevState) => Array.from(new Set([...prevState])));
       setCards((prevState) => shuffleArray<Card>([...prevState, ...prevState]));
       setShowImages(new Array(cards.length).fill(false));
     };
@@ -43,31 +49,48 @@ const Game = () => {
   }, [difficult, cards.length]);
 
   useEffect(() => {
-    const copyShowImages = [...showImages];
-
     if (selectedCards.length === 2) {
-      const areSame = selectedCards[0].id === selectedCards[1].id;
+      const areNotSame = selectedCards[0].id !== selectedCards[1].id;
 
-      if (areSame) {
-        //setShowImages(newImages);
-        return;
+      if (areNotSame) {
+        const oldStateImages = cards.map((c, i) => {
+          if (c.id === selectedCards[0].id || c.id === selectedCards[1].id)
+            return false;
+          return showImages[i];
+        });
+
+        setTimeout(() => {
+          setShowImages(oldStateImages);
+          setSelectedCards([]);
+        }, timeToShowImage);
       } else {
-        setShowImages(copyShowImages);
         setSelectedCards([]);
-        alert('igual a 2');
-        return;
       }
     }
-  }, [selectedCards]);
+
+    const win = showImages.every((v) => v);
+    win &&
+      toast('Congratulations! you won', {
+        theme: 'light',
+        type: 'success',
+        style: { fontSize: '1.8rem' },
+      });
+  }, [selectedCards, showImages, cards, timeToShowImage]);
 
   return (
     <section className="game-container">
       <ul className="cards-container">
         {cards.map((card, index) => (
           <li
-            className="hide card"
+            className={`hide card ${showImages[index] ? 'found-pair' : ''} ${
+              selectedCards.length === 2 ? 'block-click' : ''
+            }`}
             key={uuid()}
-            onClick={() => handleClickCard(index, card)}
+            onClick={(e) =>
+              showImages[index]
+                ? e.preventDefault()
+                : handleClickCard(index, card)
+            }
           >
             {showImages[index] ? card.emoji : ''}
           </li>
@@ -75,7 +98,7 @@ const Game = () => {
       </ul>
       <button
         type="button"
-        onClick={() => setDifficult(generateRandomNumber(24, 64))}
+        onClick={() => setDifficult(generateRandomNumber(minGame, maxGame))}
         className="start-game-button"
       >
         Generate a random game
